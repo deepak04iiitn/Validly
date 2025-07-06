4cd8481c517eb57bb18e5dc4ae39198899f66a7e
Deepak Kumar Yadav <dky422003@gmail.comm>
2025-07-06 23:37:34 +0530

    basic frontend routes steup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { BrowserRouter , Routes , Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Footer from './components/Footer';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
 
 export default function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <BrowserRouter>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <div className="flex-grow">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/sign-in' element={<SignIn />} />
+            <Route path='/sign-up' element={<SignUp />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
 }
